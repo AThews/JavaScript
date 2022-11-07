7f344b9312a0d60cd507725285ef8c12105b6f29
Matheus Alves de Araujo <101357346+AThews@users.noreply.github.com>
2022-11-07 02:32:46 -0300

    metade do curso :)

---
+++ aula11/ex002.js
@@ -0,0 +1,8 @@
+let pais = 'França'
+
+console.log (`Nascido no ${pais}`)
+if (pais == 'Brasil'){
+    console.log('Você é Brasileiro')
+} else {
+    console.log ('Você é Estrangeiro!')
+}
---
+++ aula12/exestra.js
@@ -0,0 +1,18 @@
+let permissao;
+
+switch (permissao) {
+  case "comum":
+    console.log("Usúrario comum");
+    break;
+
+    case "gerente":
+    console.log("Usúrario gerente");
+    break;
+
+    case "diretor":
+    console.log("Usúrario diretor");
+    break;
+
+    default:
+        console.log('Usuario não reconhecido!');
+}
