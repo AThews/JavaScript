0552feb30495f8b5b35906a2191f59332a93ffee
Matheus Alves de Araujo <101357346+AThews@users.noreply.github.com>
2022-11-16 22:49:49 -0300

    Create ambiente.js

---
+++ aula13/ambiente.js
@@ -0,0 +1,12 @@
+// console.log('Tudo bem?');
+
+var cont = 1
+// while (cont <= 6){
+//     console.log(`Passo ${cont}`)
+//     cont++ //c = c + 1
+// }
+
+do {
+    console.log(`Passo ${cont}`)
+    cont++
+} while (cont <= 6)
